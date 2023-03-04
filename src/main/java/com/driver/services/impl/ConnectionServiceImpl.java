2918bf5d22c9db0c92a9261948af66f66c2cb67a
KaushalJoshi977 <kaushaljoshi977@gmail.com>
2023-03-04 22:23:17 +0530

    first

--- src/main/java/com/driver/services/impl/ConnectionServiceImpl.java
+++ src/main/java/com/driver/services/impl/ConnectionServiceImpl.java
@@ -21,14 +21,94 @@ public class ConnectionServiceImpl implements ConnectionService {
 
     @Override
     public User connect(int userId, String countryName) throws Exception{
+        User user = userRepository2.findById(userId).get();
+        List<ServiceProvider> serviceProvider = user.getServiceProviderList();
+
+        boolean canConnect = false;
+        int id = 0;
+        CountryName countryName1 = CountryName.valueOf(countryName);
+
+
+        if (user == null) {
+            throw new Exception("User not found");
+        }
+        if (user.getConnected()) {
+            throw new Exception("Already connected");
+        }
+        if (user.getCountry().getCountryName().equals(countryName1)) {
+            return user;
+        }
+        for (ServiceProvider s: serviceProvider) {
+            for (Country c:  s.getCountryList()) {
+                if(c.getCountryName() == countryName1){
+                    canConnect = true;
+                    id = s.getId();
+                }
+            }
+
+        }
+        if (canConnect == false) throw new Exception("Unable to connect");
+
+        String maskedIp = countryName1.toCode() + "." + id + "." + userId;
+        user.setMaskedIp(maskedIp);
+        user.setConnected(true);
+
+        Connection connection = new Connection();
+        connection.setUser(user);
+        connection.setServiceProvider(serviceProviderRepository2.findById(id).get());
+        List<Connection> connectionList = user.getConnectionList();
+        connectionList.add(connection);
+        user.setConnectionList(connectionList);
+
+        userRepository2.save(user);
+        return user;
 
     }
     @Override
     public User disconnect(int userId) throws Exception {
-
+            User user = userRepository2.findById(userId).get();
+            if(!user.getConnected()) throw new Exception("Already disconnected");
+            user.setConnected(false);
+            user.setMaskedIp(null);
+            user.setConnectionList(null);
+            userRepository2.save(user);
+            return user;
     }
     @Override
     public User communicate(int senderId, int receiverId) throws Exception {
+        User sender = userRepository2.findById(senderId).orElseThrow(() -> new Exception("Sender not found"));
+        User receiver = userRepository2.findById(receiverId).orElseThrow(() -> new Exception("Receiver not found"));
 
+        // Check if sender is already connected to the current country of the receiver
+        CountryName receiverCurrentCountry = receiver.getCountry().getCountryName();
+        if (sender.getCountry().getCountryName() == receiverCurrentCountry) {
+            return sender;
+        }
+
+        // Check if the receiver is connected to a VPN, if yes then use that country for communication
+        CountryName receiverConnectedCountry = receiver.getMaskedIp() != null ?
+                CountryName.valueOf(receiver.getMaskedIp().split("\\.")[0]) : receiverCurrentCountry;
+
+        // Find a suitable VPN for the sender to connect to in order to communicate with the receiver
+        List<ServiceProvider> serviceProviders = sender.getServiceProviderList();
+        ServiceProvider suitableProvider = null;
+        for (ServiceProvider provider : serviceProviders) {
+            if (provider.getCountryList().stream().anyMatch(country -> country.getCountryName() == receiverConnectedCountry)) {
+                if (suitableProvider == null || suitableProvider.getId() > provider.getId()) {
+                    suitableProvider = provider;
+                }
+            }
+        }
+        if (suitableProvider == null) {
+            throw new Exception("Cannot establish communication");
+        }
+
+        // Connect sender to suitable VPN
+        CountryName senderNewCountry = suitableProvider.getCountryList().stream()
+                .filter(country -> country.getCountryName() == receiverCurrentCountry).findFirst()
+                .orElseThrow(() -> new Exception("Cannot establish communication")).getCountryName();
+        connect(senderId, senderNewCountry.toCode());
+
+        return userRepository2.findById(senderId).get();
     }
 }
--- src/main/java/com/driver/services/impl/UserServiceImpl.java
+++ src/main/java/com/driver/services/impl/UserServiceImpl.java
@@ -1,11 +1,17 @@
 package com.driver.services.impl;
 
+import com.driver.model.Country;
+import com.driver.model.CountryName;
+import com.driver.model.ServiceProvider;
+import com.driver.model.User;
 import com.driver.repository.CountryRepository;
 import com.driver.repository.ServiceProviderRepository;
 import com.driver.repository.UserRepository;
 import com.driver.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -19,11 +25,33 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User register(String username, String password, String countryName) throws Exception{
+        User user = new User();
+        user.setPassword(password);
+        user.setUsername(username);
+
+        Country country = new Country();
+        CountryName countryName1 = CountryName.valueOf(countryName);
+        country.setCountryName(countryName1);
+        country.setCode(countryName1.toCode());
+
+        user.setCountry(country);
+        user.setConnected(false);
+        user.setMaskedIp(null);
+        String originalIp = user.getId() + "." + countryName1.toCode();
+        user.setOriginalIp(originalIp);
+
+        userRepository3.save(user);
+
+        return user;
 
     }
 
     @Override
     public User subscribe(Integer userId, Integer serviceProviderId) {
-
+        User user = userRepository3.findById(userId).get();
+        List<ServiceProvider> serviceProviderList1 = user.getServiceProviderList();
+        serviceProviderList1.add(serviceProviderRepository3.findById(serviceProviderId).get());
+        userRepository3.save(user);
+        return user;
     }
 }
